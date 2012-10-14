ef48a3f339c23aada20919fb31791428599ee733
Bradley Rosenfeld <rosenfeld.bradley@gmail.com>
2012-10-14 14:38:42 -0400

    Now draws black pieces

--- board.py
+++ board.py
@@ -46,35 +46,44 @@ def draw(size) :
     board.forward(size)
     board.right(90)
     board.forward(size / 2)
-    for i in range(12) :
-        #set the color of the circle
-        board.color("white")
-        board.fillcolor("red")
-        radius = size // 2
-        board.down()
-        #fill in the circle with the color
-        board.begin_fill()
-        circ = 2*3.14159*radius
-        Len = circ / radius
-        turnAngle = 360 / radius
-        #start looping around the circle
-        for r in range(radius) :
-            board.forward(Len)
-            board.right(turnAngle)
-        board.end_fill()
-        board.up()
-        if (i == 3) :
-            board.forward(size)
-            board.left(180)
-        elif (i == 7) :
-            board.forward(size)
-            board.right(90)
-            board.forward(size * 2)
-            board.right(90)
-        else :
-            board.forward(size * 2)
-
+    color = "red"
+    for i in range(2) :
+        for i in range(12) :
+            #set the color of the circle
+            board.color("white")
+            board.fillcolor(color)
+            radius = size // 2
+            board.down()
+            #fill in the circle with the color
+            board.begin_fill()
+            circ = 2*3.14159*radius
+            Len = circ / radius
+            turnAngle = 360 / radius
+            #start looping around the circle
+            for r in range(radius) :
+                board.forward(Len)
+                board.right(turnAngle)
+            board.end_fill()
+            board.up()
+            if (i == 3) :
+                board.forward(size)
+                board.left(180)
+            elif (i == 7) :
+                board.forward(size)
+                board.right(90)
+                board.forward(size * 2)
+                board.right(90)
+            else :
+                board.forward(size * 2)
+        #getting fancy
+        color = "black"
+        #this positions the turtle at the bottom of the top right red square
+        board.goto(halfS - (size / 2), halfS - size)
+        #this will cause the turtle to draw in reverse
+        board.left(180)
     #show the changes
+    #reset position of turtle
+    board.goto(0, 0)
     board.tracer(1, 1)
 
 size = 60
