1928e54814b9659985ee28758fc22440d1e1536e
Bradley Rosenfeld <rosenfeld.bradley@gmail.com>
2012-10-16 00:37:27 -0400

    Fixed everything!

--- main.py
+++ main.py
@@ -34,7 +34,9 @@ def drawPiece(t, x, y, size, color) :
     t.goto(x + (0.5*size), y - (0.12 * size))
     t.setheading(0)
     t.down()
-    t.color(color)
+    t.width(2)
+    t.color("#200000")
+    t.fillcolor(color)
     t.begin_fill()
     pos = t.position()
     circumference= 2 * 3.14159 * radius
@@ -44,17 +46,18 @@ def drawPiece(t, x, y, size, color) :
         t.forward(Len)
         t.right(turnAngle)
     t.end_fill()
+    t.width(1)
 def drawDarkLightRow(t,x,y,size):
     for i in range(4):
-        drawSquare(t,x,y,size,"black")
+        drawSquare(t,x,y,size,"#D18B47")
         x=x+size
-        drawSquare(t,x,y,size,"#009900")
+        drawSquare(t,x,y,size,"#FFCE9E")
         x=x+size
 def drawLightDarkRow(t,x,y,size):
     for i in range(4):
-        drawSquare(t,x,y,size,"#009900")
+        drawSquare(t,x,y,size,"#FFCE9E")
         x=x+size
-        drawSquare(t,x,y,size,"black")
+        drawSquare(t,x,y,size,"#D18B47")
         x=x+size
 def drawCheckerBoard(t,x,y,size):
     for i in range(4):
@@ -67,6 +70,7 @@ def drawCheckerBoard(t,x,y,size):
     x = -3*size
     t.goto(x, y)
     color = "white"
+    state = 1
     for side in range(2) :
         for i in range(3) :
             for r in range(4) :
@@ -74,23 +78,25 @@ def drawCheckerBoard(t,x,y,size):
                 x = x + (size * 2)
             y = y - size
             if (i % 2 == 0) :
-                x = -(4 + side)*size
+                x = -(3 + state)*size
             else :
                 x = -(3 + side)*size
-        color = "red"
+        color = "#C40003"
         y = -size
         x = -4*size
+        state = 0
 
 def moveChecker(t, size, fromRow,fromCol,toRow,toCol) :
     fromX = getCoordX(fromCol, size)
     fromY = getCoordY(fromRow, size)
     if (fromX % 2 == 0 and fromY % 2 != 0) :
-        color = "black"
+        color = "#FFCE9E"
     else :
-        color = "#009900"
+        color = "#D18B47"
     drawSquare(t, fromX, fromY, size, color)
     toX = getCoordX(toCol, size)
     toY = getCoordY(toRow, size)
+    #actual color will be calculated eventually
     drawPiece(t, toX, toY, size, "red")
     
 def checkers(t, size):
